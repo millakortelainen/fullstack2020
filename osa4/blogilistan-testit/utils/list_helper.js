d59e47001a2ed9d3dc2911a275f28da8985222c4
Milla <milla.kortelainen@helsinki.fi>
2020-07-02 15:35:19 +0300

    update

---
+++ osa4/blogilistan-testit/utils/list_helper.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+const { groupBy } = require('lodash');
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogs.length === 0
+        ? 0
+        : blogs.map(blog => blog.likes).reduce(reducer)
+}
+
+const favoriteBlog = (blogs) => {
+    const mostLikes = Math.max(...blogs.map(b => b.likes))
+
+    return blogs.find(blog => blog.likes === mostLikes)
+}
+
+const groupByAuthor2 = (blogs) => {
+    return _.chain(blogs)
+        .groupBy("author")
+        .map((value, key) => ({ author: key, likes: totalLikes(value) }))
+        .value()
+}
+
+const mostBlogs = (blogs) => {
+    return _.chain(blogs)
+    .groupBy("author")
+    .map((value, key) => ({ author: key, blogs: value.length }))
+    .maxBy("blogs")
+    .value()
+}
+
+const mostLikes = (blogs) => {
+    return _.chain(blogs)
+    .groupBy("author")
+    .map((value, key) => ({ author: key, likes: totalLikes(value) }))
+    .maxBy("likes")
+    .value()
+    //return _.maxBy(groupByAuthor2(blogs), "likes")
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
